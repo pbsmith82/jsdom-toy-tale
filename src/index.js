0c9b7317a6b382b64606c6b67034c3fa93459eb6
pbsmith82 <pbsmith82@gmail.com>
2021-03-28 13:05:42 -0400

    commit tree ec7b59be975bb507c9ff419a285e6a9817ac2302

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-let addToy = false;
+let addToy = false
+const toyUrl = 'http://localhost:3000/toys' 
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -12,4 +13,23 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+const toyImageContainer = document.querySelector("#toy-collection")
+  fetch(toyUrl)
+    .then(function(response){
+      return response.json()
+    }).then(function(json){
+      renderCards(json)
+    })
+  function renderCards(toys){
+  toys.forEach(object => {
+    const card = document.createElement('div')
+    card.className = "card"
+    card.innerHTML = `<img src="${object.image}" class="toy-avatar"> <br> ID: ${object.id} <br> Name: ${object.name} <br> Likes: ${object.likes}` 
+    toyImageContainer.append(card)
+  })
+  }
+})
